93ad61ebbaf62e83d6f16fa796180950f868b3c4
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-13 23:06:03 -0300

    navbar fully created and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,6 +28,18 @@ const Navbar = () => {
             onClick={() => setToggle((prev) => !prev)}
             //em react nao é correto atualizar o estado diretamente com o estado anterior, deve ser usada uma callback function para tal
           />
+
+          <div className={`${toggle ? 'flex' : 'hidden '} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+            <ul className='list-none flex flex-col justify-end items-center flex-1'>
+              {navLinks.map((nav, index) => (
+                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
+                  <a href={`#${nav.id}`}>
+                    {nav.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
       </div>
     </nav>
   )
