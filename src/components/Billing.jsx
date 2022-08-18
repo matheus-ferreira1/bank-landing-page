c434251e7d373a1da9cf6afc7df998d43365cfb7
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-17 21:40:04 -0300

    billing component finalized

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,6 +10,23 @@ const Billing = () => (
       <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
       <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
     </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className='sm:block hidden' /> billing & invoicing
+      </h2>
+
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptates expedita ipsum inventore corporis. Ratione, cumque sit? Natus, voluptatibus possimus! Perspiciatis quis odio rem inventore maiores aperiam, veritatis dignissimos reiciendis?
+      </p>
+
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+        <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+        <img src={google} alt="google_play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+
+      </div>
+
+    </div>
   </section>
 )
 
