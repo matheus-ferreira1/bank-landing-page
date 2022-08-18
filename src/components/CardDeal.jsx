521bd10aa904b715673487a881d2307031f9260d
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-17 21:44:01 -0300

    carddeal component initialized

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../styles';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2>
+        Find a better card deal <br className='sm:block hidden' /> in few easy steps
+      </h2>
+    </div>
+
+  </section>
+)
+
 
 export default CardDeal
