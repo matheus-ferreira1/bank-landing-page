b5f9dc9f70ecc4f4048e48b99a57a522964924d7
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-19 17:29:35 -0300

    button component received a border radius

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,9 +6,15 @@ import Button from './Button';
 const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
-      <h2>
+      <h2 className={styles.heading2}>
         Find a better card deal <br className='sm:block hidden' /> in few easy steps
       </h2>
+
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora minus repudiandae laborum, ullam excepturi quasi sint tempore sequi iste?
+      </p>
+
+      <Button styles="mt-10" /> 
     </div>
 
   </section>
