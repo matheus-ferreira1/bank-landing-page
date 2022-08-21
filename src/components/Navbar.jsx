c3fb7df3bad225763175482655d4ed87c1de24f4
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-21 13:52:32 -0300

    correction on the navbar z-index and added color on the active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,42 +4,54 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants'
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false)
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className='w-full flex py-6 justify-between items-center nabar'>
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32px] ' />
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+          <li
+            key={nav.id}
+            className={`z-[100000] font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
 
-      <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img
-            src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
-            onClick={() => setToggle((prev) => !prev)}
-            //em react nao é correto atualizar o estado diretamente com o estado anterior, deve ser usada uma callback function para tal
-          />
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
 
-          <div className={`${toggle ? 'flex' : 'hidden '} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-            <ul className='list-none flex flex-col justify-end items-center flex-1'>
-              {navLinks.map((nav, index) => (
-                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
-                  <a href={`#${nav.id}`}>
-                    {nav.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )
